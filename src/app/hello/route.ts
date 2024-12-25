5fa36353840847137de960842dcf9a4035f2fd6d
Siddharth20050904 <siddharthsuryavanshi0409@gmail.com>
2024-12-25 14:52:35 +0530

    connected to db

--- src/app/hello/route.ts
+++ src/app/hello/route.ts
@@ -1,7 +1,18 @@
-const users = [
-    {id: 1, name: 'John Doe'},
-];
+import { PrismaClient } from '@prisma/client'
 
-export async function GET() {
-    return Response.json(users);
+const prisma = new PrismaClient()
+
+async function main() {
+  const allUsers = await prisma.user.findMany()
+  console.log(allUsers)
 }
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
